ff630479417d04a2b93f843f359c44e16e33ddc4
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-05-03 16:01:07 +0200

    Update the function for getting random images

--- components/AnimalPairs.tsx
+++ components/AnimalPairs.tsx
@@ -12,83 +12,60 @@ export default function AnimalPairs() {
   }, [])
 
   const handleSelectElements = () => {
-    const dogs = images.dogs
-    const cows = images.cows
-    const pigs = images.pigs
-    const rabbits = images.rabbits
+    const animals = [images.dogs, images.cows, images.pigs, images.rabbits]
     const fillers = images.fillers
-    const firstDogImages = getRandomAnimals(dogs, 6)
-    const firstCowImages = getRandomAnimals(cows, 6)
-    const firstPigImages = getRandomAnimals(pigs, 6)
-    const firstRabbitImages = getRandomAnimals(rabbits, 6)
+    const firstAnimalImages = animals.map((animal) =>
+      getRandomAnimals(animal, 6)
+    )
     const firstFillerImages = getRandomFillers(fillers, 8)
-    const firstNewDogs = dogs.map((dog) =>
-      dog.filter((d) => !firstDogImages.includes(d))
-    )
-    const firstNewCows = cows.map((cow) =>
-      cow.filter((c) => !firstCowImages.includes(c))
-    )
-    const firstNewPigs = pigs.map((pig) =>
-      pig.filter((p) => !firstPigImages.includes(p))
-    )
-    const firstNewRabbits = rabbits.map((rabbit) =>
-      rabbit.filter((r) => !firstRabbitImages.includes(r))
+    const firstNewAnimals = animals.map((animalPairs, index) =>
+      animalPairs.map((animalPair) =>
+        animalPair.filter((a) => !firstAnimalImages[index].includes(a))
+      )
     )
     const firstNewFillers = fillers.filter(
-      (f) => !firstFillerImages.includes(f)
+      (filler) => !firstFillerImages.includes(filler)
     )
-    const firstPairImages = [
-      ...firstDogImages,
-      ...firstCowImages,
-      ...firstPigImages,
-      ...firstRabbitImages,
-    ]
     setFirstTaskImages(
-      [...firstPairImages, ...firstFillerImages].sort(() => Math.random() - 0.5)
-    )
-    const secondPairImages = firstPairImages.map((animal) => {
-      const lastChar = animal[animal.length - 1]
-      const newLastChar = lastChar === "a" ? "b" : "a"
-      return animal.slice(0, -1) + newLastChar
-    })
-    const secondNewDogs = firstNewDogs
-      .map((dog) => dog.filter((d) => !secondPairImages.includes(d)))
-      .filter((d) => d.length > 0)
-    const secondNewCows = firstNewCows
-      .map((cow) => cow.filter((c) => !secondPairImages.includes(c)))
-      .filter((c) => c.length > 0)
-    const secondNewPigs = firstNewPigs
-      .map((pig) => pig.filter((p) => !secondPairImages.includes(p)))
-      .filter((p) => p.length > 0)
-    const secondNewRabbits = firstNewRabbits
-      .map((rabbit) => rabbit.filter((r) => !secondPairImages.includes(r)))
-      .filter((r) => r.length > 0)
-    const secondDogImages = getRandomAnimals(secondNewDogs, 6)
-    const secondCowImages = getRandomAnimals(secondNewCows, 6)
-    const secondPigImages = getRandomAnimals(secondNewPigs, 6)
-    const secondRabbitImages = getRandomAnimals(secondNewRabbits, 6)
-    const secondFillerImages = getRandomFillers(firstNewFillers, 8)
-    const secondOddImages = [
-      ...secondDogImages,
-      ...secondCowImages,
-      ...secondPigImages,
-      ...secondRabbitImages,
-    ]
-    setSecondTaskImages(
-      [...secondPairImages, ...secondOddImages, ...secondFillerImages].sort(
+      [...firstAnimalImages.flat(2), ...firstFillerImages].sort(
         () => Math.random() - 0.5
       )
     )
+    const secondAnimalImages = firstAnimalImages.map((animalPairs) =>
+      animalPairs.map((animalPair) => {
+        const lastChar = animalPair[animalPair.length - 1]
+        const newLastChar = lastChar === "a" ? "b" : "a"
+        return animalPair.slice(0, -1) + newLastChar
+      })
+    )
+    const secondNewAnimals = firstNewAnimals.map((animalPairs, index) =>
+      animalPairs
+        .map((animalPair) =>
+          animalPair.filter((a) => !secondAnimalImages[index].includes(a))
+        )
+        .filter((animalPairs) => animalPairs.length > 0)
+    )
+    const secondOddImages = secondNewAnimals.map((animal) =>
+      getRandomAnimals(animal, 6)
+    )
+    const secondFillerImages = getRandomFillers(firstNewFillers, 8)
+    setSecondTaskImages(
+      [
+        ...secondAnimalImages.flat(2),
+        ...secondOddImages.flat(2),
+        ...secondFillerImages,
+      ].sort(() => Math.random() - 0.5)
+    )
   }
 
-  const getRandomAnimals = (animalArr, count) => {
+  const getRandomAnimals = (animalArr: string[][], count: number): string[] => {
     const shuffled = animalArr
       .map((animal) => animal[Math.floor(Math.random() * 2)])
       .sort(() => Math.random() - 0.5)
     return shuffled.slice(0, count)
   }
 
-  const getRandomFillers = (fillerArr, count) => {
+  const getRandomFillers = (fillerArr: string[], count: number): string[] => {
     const shuffled = fillerArr.sort(() => Math.random() - 0.5)
     return shuffled.slice(0, count)
   }
