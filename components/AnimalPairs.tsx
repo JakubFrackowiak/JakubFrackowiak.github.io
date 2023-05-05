bb112119dd0fddc180b79a980f2469cd6475d9f6
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-05-05 18:47:44 +0200

    Update the functions for reading image names from storage, add function for displaying new image every 3 seconds

--- components/AnimalPairs.tsx
+++ components/AnimalPairs.tsx
@@ -1,34 +1,99 @@
 import { Box } from "@mui/material"
 import { Stack } from "@mui/system"
 import { useEffect, useState } from "react"
-import { useStorage } from "reactfire"
-import { setRandomImages } from "images"
+import { StorageImage, useStorage, useStorageDownloadURL } from "reactfire"
+import { getImageNames } from "images"
 
 export default function AnimalPairs() {
   const [firstTaskImages, setFirstTaskImages] = useState([])
+  const [firstTaskIndex, setFirstTaskIndex] = useState(0)
   const [secondTaskImages, setSecondTaskImages] = useState([])
+  const [secondTaskIndex, setSecondTaskIndex] = useState(0)
 
   const storage = useStorage()
 
   useEffect(() => {
-    setRandomImages(storage, setFirstTaskImages, setSecondTaskImages)
+    setRandomImages(storage)
+  }, [])
+
+  const setRandomImages = async (storage) => {
+    const { animals, fillers } = await getImageNames(storage)
+    const firstAnimalImages = animals.map((animal) =>
+      getRandomAnimals(animal, 6)
+    )
+    const firstFillerImages = getRandomFillers(fillers, 8)
+    const firstNewAnimals = animals.map((animalPairs, index) =>
+      animalPairs.map((animalPair) =>
+        animalPair.filter((a) => !firstAnimalImages[index].includes(a))
+      )
+    )
+    const firstNewFillers = fillers.filter(
+      (filler) => !firstFillerImages.includes(filler)
+    )
+    setFirstTaskImages(
+      [...firstAnimalImages.flat(2), ...firstFillerImages].sort(
+        () => Math.random() - 0.5
+      )
+    )
+    const secondAnimalImages = firstAnimalImages.map((animalPair) =>
+      animalPair.map((animal) => {
+        const animalName = animal.split(".")[0]
+        const lastChar = animalName.slice(-1)
+        const newLastChar = lastChar === "a" ? "b.jpg" : "a.jpg"
+        const newAnimalName = animalName.slice(0, -1) + newLastChar
+        return newAnimalName
+      })
+    )
+    const secondNewAnimals = firstNewAnimals.map((animalPairs, index) =>
+      animalPairs
+        .map((animalPair) =>
+          animalPair.filter((a) => !secondAnimalImages[index].includes(a))
+        )
+        .filter((animalPairs) => animalPairs.length > 0)
+    )
+    const secondOddImages = secondNewAnimals.map((animal) =>
+      getRandomAnimals(animal, 6)
+    )
+    const secondFillerImages = getRandomFillers(firstNewFillers, 8)
+    setSecondTaskImages(
+      [
+        ...secondAnimalImages.flat(2),
+        ...secondOddImages.flat(2),
+        ...secondFillerImages,
+      ].sort(() => Math.random() - 0.5)
+    )
+  }
+
+  const getRandomAnimals = (animalArr: string[][], count: number): string[] => {
+    const shuffled = animalArr
+      .map((animal) => animal[Math.floor(Math.random() * 2)])
+      .sort(() => Math.random() - 0.5)
+    return shuffled.slice(0, count)
+  }
+
+  const getRandomFillers = (fillerArr: string[], count: number): string[] => {
+    const shuffled = fillerArr.sort(() => Math.random() - 0.5)
+    return shuffled.slice(0, count)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFirstTaskIndex((firstTaskIndex) => firstTaskIndex + 1)
+    }, 3000)
+    return () => clearInterval(interval)
   }, [])
 
   return (
-    <Stack direction="row" spacing={10} mt="55rem">
-      <Stack direction="column" spacing={0}>
-        {firstTaskImages.map((animal, index) => (
-          <Box key={index}>{animal}</Box>
-        ))}
-        {firstTaskImages.length}
-      </Stack>
-      <Stack direction="column" spacing={0}>
-        {secondTaskImages.map((animal, index) => (
-          <Box key={index}>{animal}</Box>
-        ))}
-        {secondTaskImages.length}
-      </Stack>
-      <Stack direction="column" spacing={2}></Stack>
+    <Stack direction="row" spacing={10}>
+      {firstTaskImages && firstTaskImages.length > 0 ? (
+        <Box>
+          <StorageImage
+            storagePath={firstTaskImages[firstTaskIndex]}
+            width={200}
+            height={200}
+          />
+        </Box>
+      ) : null}
     </Stack>
   )
 }
--- images.ts
+++ images.ts
@@ -11,13 +11,15 @@ export const getImageNames = async (storage) => {
     categories.map(async (category) => {
       const categoryRef = ref(storage, category)
       const imageNameItems = await listAll(categoryRef)
-      const imageNames = imageNameItems.items.map((item) => item.name)
+      const imageNames = imageNameItems.items.map(
+        (item) => category + "/" + item.name
+      )
       animals.push(imageNames)
     })
   )
   const fillersRef = ref(storage, "fillers")
   const fillersItems = await listAll(fillersRef)
-  const fillers = fillersItems.items.map((item) => item.name)
+  const fillers = fillersItems.items.map((item) => "fillers/" + item.name)
   const animalsSliced = animals.map((animalArr) =>
     animalArr.map((image) => image.split(".")[0].slice(0, -1))
   )
@@ -25,68 +27,9 @@ export const getImageNames = async (storage) => {
     animalArr.filter((v, i, a) => a.indexOf(v) === i)
   )
   const animalsPaired = animalsSets.map((animalArr) =>
-    animalArr.map((image) => [image + "a", image + "b"])
+    animalArr.map((image) => [image + "a.jpg", image + "b.jpg"])
   )
+
   const images = { animals: animalsPaired, fillers }
   return images
-}
-
-export const setRandomImages = async (
-  storage,
-  setFirstTaskImages,
-  setSecondTaskImages
-) => {
-  const { animals, fillers } = await getImageNames(storage)
-  const firstAnimalImages = animals.map((animal) => getRandomAnimals(animal, 6))
-  const firstFillerImages = getRandomFillers(fillers, 8)
-  const firstNewAnimals = animals.map((animalPairs, index) =>
-    animalPairs.map((animalPair) =>
-      animalPair.filter((a) => !firstAnimalImages[index].includes(a))
-    )
-  )
-  const firstNewFillers = fillers.filter(
-    (filler) => !firstFillerImages.includes(filler)
-  )
-  setFirstTaskImages(
-    [...firstAnimalImages.flat(2), ...firstFillerImages].sort(
-      () => Math.random() - 0.5
-    )
-  )
-  const secondAnimalImages = firstAnimalImages.map((animalPairs) =>
-    animalPairs.map((animalPair) => {
-      const lastChar = animalPair[animalPair.length - 1]
-      const newLastChar = lastChar === "a" ? "b" : "a"
-      return animalPair.slice(0, -1) + newLastChar
-    })
-  )
-  const secondNewAnimals = firstNewAnimals.map((animalPairs, index) =>
-    animalPairs
-      .map((animalPair) =>
-        animalPair.filter((a) => !secondAnimalImages[index].includes(a))
-      )
-      .filter((animalPairs) => animalPairs.length > 0)
-  )
-  const secondOddImages = secondNewAnimals.map((animal) =>
-    getRandomAnimals(animal, 6)
-  )
-  const secondFillerImages = getRandomFillers(firstNewFillers, 8)
-  setSecondTaskImages(
-    [
-      ...secondAnimalImages.flat(2),
-      ...secondOddImages.flat(2),
-      ...secondFillerImages,
-    ].sort(() => Math.random() - 0.5)
-  )
-}
-
-const getRandomAnimals = (animalArr: string[][], count: number): string[] => {
-  const shuffled = animalArr
-    .map((animal) => animal[Math.floor(Math.random() * 2)])
-    .sort(() => Math.random() - 0.5)
-  return shuffled.slice(0, count)
-}
-
-const getRandomFillers = (fillerArr: string[], count: number): string[] => {
-  const shuffled = fillerArr.sort(() => Math.random() - 0.5)
-  return shuffled.slice(0, count)
 }
