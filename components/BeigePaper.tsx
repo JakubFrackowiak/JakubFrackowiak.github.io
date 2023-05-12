be86f0e197006357db46b2df8f2f36d656ecc7cd
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-05-12 15:44:11 +0200

    Add beige paper component

---
+++ components/BeigePaper.tsx
@@ -0,0 +1,26 @@
+import { Paper } from "@mui/material"
+
+export function BeigePaper({
+  children,
+  height = "100%",
+  width = "100%",
+  p = "2rem",
+  borderRadius = "1.5rem",
+}) {
+  return (
+    <Paper
+      variant="elevation"
+      elevation={24}
+      sx={{
+        bgcolor: "#f7faf2",
+        height: height,
+        maxHeight: "60vh",
+        width: width,
+        p: p,
+        borderRadius: borderRadius,
+      }}
+    >
+      {children}
+    </Paper>
+  )
+}
