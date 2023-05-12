915aa0eecf481de1151673d5ae8eb795f166c0a1
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-05-12 15:45:15 +0200

    Improve the styling of the stepper component ```

--- pages/badanie.tsx
+++ pages/badanie.tsx
@@ -1,16 +1,38 @@
 import { FirstTask } from "components/FirstTask"
 import { SecondTask } from "components/SecondTask"
 import { Container, Stack, Step, StepLabel, Stepper } from "@mui/material"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getRandomImages } from "images"
+import { useStorage } from "reactfire"
 
 export default function Badanie() {
   const [currentTask, setCurrentTask] = useState(0)
+  const [firstTaskImages, setFirstTaskImages] = useState([])
+  const [secondTaskImages, setSecondTaskImages] = useState([])
   const steps = ["Zadanie 1", "Zadanie 2", "Zadanie 3"]
+
+  const storage = useStorage()
+
+  const getImages = async () => {
+    const { firstTaskImages: firstImages, secondTaskImages: secondImages } =
+      await getRandomImages(storage)
+    setFirstTaskImages(firstImages)
+    setSecondTaskImages(secondImages)
+  }
+
+  useEffect(() => {
+    getImages()
+  }, [])
 
   const renderTask = () => {
     switch (currentTask) {
       case 0:
-        return <FirstTask setCurrentTask={setCurrentTask} />
+        return (
+          <FirstTask
+            setCurrentTask={setCurrentTask}
+            firstTaskImages={firstTaskImages}
+          />
+        )
       case 1:
         return <SecondTask setCurrentTask={setCurrentTask} />
       case 2:
@@ -20,8 +42,17 @@ export default function Badanie() {
 
   return (
     <Container maxWidth="md">
-      <Stack height="100vh" justifyContent="center" alignItems="center">
-        <Stepper activeStep={currentTask}>
+      <Stack
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+        spacing={6}
+      >
+        <Stepper
+          activeStep={currentTask}
+          alternativeLabel
+          sx={{ width: "100%" }}
+        >
           {steps.map((label, index) => {
             const stepProps: { completed?: boolean } = {}
             return (
