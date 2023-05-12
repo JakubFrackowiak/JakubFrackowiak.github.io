6fd4dace775ddd1e679586d66f136891cad1c5d5
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-05-12 15:44:49 +0200

    Refactor first task component into two components

--- components/FirstTask.tsx
+++ components/FirstTask.tsx
@@ -1,84 +1,21 @@
-import { LinearProgress, CircularProgress } from "@mui/material"
 import { Stack } from "@mui/system"
-import { useEffect, useState } from "react"
-import { useStorage } from "reactfire"
-import { getRandomImages } from "images"
-import { getDownloadURL, ref } from "firebase/storage"
+import { useState } from "react"
 
-export function FirstTask({ setCurrentTask }) {
-  const [firstTaskImages, setFirstTaskImages] = useState([])
-  const [firstTaskURLs, setFirstTaskURLs] = useState([])
-  const [firstTaskIndex, setFirstTaskIndex] = useState(0)
-  const [secondTaskImages, setSecondTaskImages] = useState([])
-  const [secondTaskIndex, setSecondTaskIndex] = useState(0)
-  const [progress, setProgress] = useState(0)
+import { BeigeButton } from "./BeigeButton"
+import { FirstTaskImages } from "./FirstTaskImages"
 
-  const storage = useStorage()
-
-  const getImages = async () => {
-    const { firstTaskImages: firstImages, secondTaskImages: secondImages } =
-      await getRandomImages(storage)
-    setFirstTaskImages(firstImages)
-    setSecondTaskImages(secondImages)
-  }
-
-  useEffect(() => {
-    getImages()
-    const interval = setInterval(() => {
-      setFirstTaskIndex((firstTaskIndex) => firstTaskIndex + 1)
-      setProgress(0)
-    }, 3000)
-    return () => clearInterval(interval)
-  }, [])
-
-  useEffect(() => {
-    if (firstTaskIndex === 50) {
-      setCurrentTask(1)
-    }
-  }, [firstTaskIndex])
-
-  useEffect(() => {
-    let intervalId
-    if (progress < 100) {
-      intervalId = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress >= 100) {
-            clearInterval(intervalId)
-            return 100
-          }
-          return prevProgress + 1
-        })
-      }, 30) // update progress every 30 milliseconds
-    }
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [firstTaskIndex, progress])
-
-  useEffect(() => {
-    const promises = firstTaskImages.map((imageName) => {
-      const imageRef = ref(storage, imageName)
-      return getDownloadURL(imageRef)
-    })
-
-    Promise.all(promises).then((urls) => setFirstTaskURLs(urls))
-  }, [])
-
-  console.log("firstTaskImages", firstTaskURLs)
+export function FirstTask({ setCurrentTask, firstTaskImages }) {
+  const [isReady, setIsReady] = useState(false)
 
   return (
-    <Stack>
-      {firstTaskURLs && firstTaskURLs.length > 0 ? (
-        <Stack>
-          <img
-            src={firstTaskURLs[firstTaskIndex]}
-            style={{ width: "80vw", height: "50vh", objectFit: "contain" }}
-            alt="animal image"
-          />
-          <LinearProgress variant="determinate" value={progress} />
-        </Stack>
+    <Stack height="60vh" justifyContent="center">
+      {isReady ? (
+        <FirstTaskImages
+          setCurrentTask={setCurrentTask}
+          firstTaskImages={firstTaskImages}
+        />
       ) : (
-        <CircularProgress variant="indeterminate" />
+        <BeigeButton onClick={() => setIsReady(true)}>Jestem gotów</BeigeButton>
       )}
     </Stack>
   )
---
+++ components/FirstTaskImages.tsx
@@ -0,0 +1,55 @@
+import { CircularProgress, Stack } from "@mui/material"
+import { getDownloadURL, ref } from "firebase/storage"
+import { useEffect, useState } from "react"
+import { useStorage } from "reactfire"
+import { BeigePaper } from "./BeigePaper"
+
+export function FirstTaskImages({ setCurrentTask, firstTaskImages }) {
+  const [firstTaskURLs, setFirstTaskURLs] = useState([])
+  const [firstTaskIndex, setFirstTaskIndex] = useState(0)
+
+  const storage = useStorage()
+
+  useEffect(() => {
+    if (firstTaskURLs.length > 0) {
+      const interval = setInterval(() => {
+        setFirstTaskIndex((firstTaskIndex) => firstTaskIndex + 1)
+      }, 3000)
+      return () => clearInterval(interval)
+    }
+  }, [firstTaskURLs])
+
+  useEffect(() => {
+    if (firstTaskIndex === 50) {
+      setCurrentTask(1)
+    }
+  }, [firstTaskIndex])
+
+  useEffect(() => {
+    const promises = firstTaskImages.map((imageName) => {
+      const imageRef = ref(storage, imageName)
+      return getDownloadURL(imageRef)
+    })
+
+    Promise.all(promises).then((urls) => setFirstTaskURLs(urls))
+  }, [firstTaskImages])
+
+  return firstTaskURLs && firstTaskURLs.length > 0 ? (
+    <Stack>
+      <BeigePaper width="fit-content" height="60vh" p="0">
+        <img
+          src={firstTaskURLs[firstTaskIndex]}
+          style={{
+            height: "60vh",
+            width: "100%",
+            objectFit: "contain",
+            borderRadius: "1.5rem",
+          }}
+          alt="animal image"
+        />
+      </BeigePaper>
+    </Stack>
+  ) : (
+    <CircularProgress variant="indeterminate" />
+  )
+}
