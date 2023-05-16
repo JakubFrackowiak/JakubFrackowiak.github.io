a5acd6d762c91716e0208a7e30c5c3a2a43c836e
Jakub Frackowiak <xfragless@gmail.com>
2023-05-16 18:54:58 +0200

    refactor ThirdTask component

--- components/ThirdTask.tsx
+++ components/ThirdTask.tsx
@@ -2,13 +2,50 @@ import { Stack } from "@mui/material"
 import { CopyID } from "./CopyID"
 import { Question } from "./Question"
 import { ThirdTaskImages } from "./ThirdTaskImages"
+import { BeigeButton } from "./BeigeButton"
+import { useSurveyStore } from "storage/survey-store"
+import { useState } from "react"
 
 export function ThirdTask() {
+  const [imageAnswers, setImageAnswers] = useState([])
+  const { setThirdTaskIndex, setThirdTaskAnswers, thirdTaskAnswers } =
+    useSurveyStore((state) => ({
+      thirdTaskAnswers: state.thirdTaskAnswers,
+      setThirdTaskIndex: state.setThirdTaskIndex,
+      setThirdTaskAnswers: state.setThirdTaskAnswers,
+    }))
+
+  const questions = [
+    "Czy waliles koniucha do nagich zdiec swojej matki?",
+    "Czy waliles koniucha do nagich zdiec swojej matki?",
+    "Czy waliles koniucha do nagich zdiec swojej matki?",
+    "Czy waliles koniucha do nagich zdiec swojej matki?",
+    "Czy waliles koniucha do nagich zdiec swojej matki?",
+  ]
+
+  const handleClick = () => {
+    setThirdTaskIndex()
+    setThirdTaskAnswers(imageAnswers.flat(1))
+  }
+  console.log("stringi wiktorii hofmokl", thirdTaskAnswers)
+
   return (
     <Stack width="100%">
       <ThirdTaskImages />
-      <Question />
+      {questions.map((question, questionIndex) => (
+        <Question
+          question={question}
+          imageAnswers={imageAnswers}
+          questionIndex={questionIndex}
+          setImageAnswers={setImageAnswers}
+        />
+      ))}
       <CopyID />
+      <Stack py="2rem">
+        <BeigeButton onClick={() => handleClick()}>
+          Następne zdjęcie
+        </BeigeButton>
+      </Stack>
     </Stack>
   )
 }
