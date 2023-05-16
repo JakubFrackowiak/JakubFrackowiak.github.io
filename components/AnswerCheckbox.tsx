bbf7c54b16d45007550e5adc4c466ff827f10130
Jakub Frackowiak <xfragless@gmail.com>
2023-05-16 18:54:06 +0200

    refactor AnswerCheckbox component

--- components/AnswerCheckbox.tsx
+++ components/AnswerCheckbox.tsx
@@ -1,47 +1,66 @@
 import CircleIcon from "@mui/icons-material/Circle"
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked"
-import { Checkbox, FormControlLabel, Stack, Typography } from "@mui/material"
+import { Checkbox, FormControlLabel, Typography } from "@mui/material"
 import { grey } from "@mui/material/colors"
-import { useState } from "react"
+import { useEffect } from "react"
+import { useSurveyStore } from "storage/survey-store"
 
-export function AnswerCheckbox({ answers: answers }) {
-  const [selectedValue, setSelectedValue] = useState("")
+export function AnswerCheckbox({
+  setImageAnswers,
+  questionIndex,
+  answer,
+  imageAnswers,
+}) {
+  const { thirdTaskIndex } = useSurveyStore((state) => ({
+    thirdTaskIndex: state.thirdTaskIndex,
+  }))
+
+  useEffect(() => {
+    setImageAnswers((prev) => {
+      const newAnswers = [...prev]
+      newAnswers[thirdTaskIndex] = Array(4).fill(null)
+      return newAnswers
+    })
+  }, [thirdTaskIndex])
 
   const handleChange = (event) => {
-    setSelectedValue(event.target.value)
+    setImageAnswers((prev) => {
+      const newAnswers = [...prev]
+      newAnswers[thirdTaskIndex][questionIndex] = answer.label
+      return newAnswers
+    })
   }
-
   return (
-    <Stack direction="row" width="100%" justifyContent="space-between">
-      {answers.map((answer) => (
-        <FormControlLabel
-          sx={{
-            position: "relative",
-          }}
-          checked={selectedValue === answer.label}
-          onChange={handleChange}
-          key={answer.label}
-          value={answer.label}
-          control={
-            <Checkbox
-              checkedIcon={
-                <CircleIcon
-                  sx={{
-                    color: grey[700],
-                  }}
-                />
-              }
-              icon={<RadioButtonUncheckedIcon />}
+    <FormControlLabel
+      sx={{
+        position: "relative",
+      }}
+      checked={
+        imageAnswers.length > 0 && imageAnswers[thirdTaskIndex] != null
+          ? imageAnswers[thirdTaskIndex][questionIndex] == answer.label
+          : false
+      }
+      onChange={handleChange}
+      key={answer.label}
+      value={answer.label}
+      control={
+        <Checkbox
+          checkedIcon={
+            <CircleIcon
+              sx={{
+                color: grey[700],
+              }}
             />
           }
-          label={
-            <Typography noWrap position="absolute" bottom="-1.5rem">
-              {answer.label}
-            </Typography>
-          }
-          labelPlacement="bottom"
+          icon={<RadioButtonUncheckedIcon />}
         />
-      ))}
-    </Stack>
+      }
+      label={
+        <Typography noWrap position="absolute" bottom="-1.5rem">
+          {answer.label}
+        </Typography>
+      }
+      labelPlacement="bottom"
+    />
   )
 }
